1169f63f3535840d46f9b9b62b608111a6aacfa7
sualsa <su.alba@hotmail.com>
2023-07-20 05:24:12 +0200

    REACT Ejemplo basico ROUTES

---
+++ src/pages/Default.js
@@ -0,0 +1,6 @@
+const Default = () => {
+    return (
+      <h1>Default</h1>
+    )
+  }
+  export default Default;
---
+++ src/pages/Layout.js
@@ -0,0 +1,23 @@
+const { Link, Outlet } = require("react-router-dom")
+
+const Layout = () => {
+    return <div>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/dashboard">Dashboard</Link>
+                </li>
+            </ul>
+        </nav>
+        <hr/>
+        <Outlet/>
+    </div>
+}
+
+export default Layout;
